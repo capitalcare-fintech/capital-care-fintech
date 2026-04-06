5e0e81dfb365291afdb06e65b3b2aaa1f6d75d9c
Arsh Sheikh <151069384+arshsheikh87@users.noreply.github.com>
2026-04-07 00:06:56 +0530

    kat gya

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["jspdf"],
-<<<<<<< HEAD
     // output: 'export',
 
-=======
->>>>>>> 6ec99ae336085601d72029eb4bc9e8127e2f7f4d
 };
 
 export default nextConfig;
